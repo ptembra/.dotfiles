5144ee169ede192b439797282a86473341b784ff
ptembra <ph.tembra@gmail.com>
2025-10-08 09:32:33 -0400

    changed nvim config to have transparent background

--- nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/lua/plugins/ui/colorscheme.lua
@@ -6,7 +6,9 @@ return {
         priority = 1000, -- make sure to load this before all the other start plugins
         -- Optional; default configuration will be used if setup isn't called.
         config = function()
-            require("everforest").setup()
+            require("everforest").setup({
+                transparent_background_level = 2
+            })
         end,
     }
 }
